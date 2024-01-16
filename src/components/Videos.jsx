f90161b1dee57d8f9aa74181536c46aaf81594b3
elian-dev <geordymemdoza@gmail.com>
2024-01-16 00:18:05 -0600

    Channel cards

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,15 +4,15 @@ import { VideoCard, ChannelCard } from './'
 const Videos = ({ videos }) => {
   console.log(videos)
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
-      {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
-          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
-        </Box>
-      ))}
-    </Stack>
-  )
+		<Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+			{videos.map((item, idx) => (
+				<Box key={idx}>
+					{item.id.videoId && <VideoCard video={item} />}
+					{item.id.channelId && <ChannelCard channelDetail={item} />}
+				</Box>
+			))}
+		</Stack>
+	);
 }
 
 export default Videos
