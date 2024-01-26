27f296e74bbafc95e5520929ea85e88cdeb7a808
elian-dev <geordymemdoza@gmail.com>
2024-01-25 22:20:10 -0600

    feat: :sparkles: Search feed implemented

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,30 +4,44 @@ import { Paper, IconButton } from '@mui/material'
 import Search from '@mui/icons-material/Search'
 
 const SearchBar = () => {
-  return (
-    <Paper
-      component='form'
-      onSubmit={() => {}}
-      sx={{
-        borderRadius: 20,
-        sx: '1px solid #000',
-        pl: 2,
-        boxShadow: 'none',
-        mr: { sm: 5 }
-      }}
-    >
-      <input
-        type='text'
-        className='search-bar'
-        placeholder='Search'
-        value=''
-        onChange={() => {}}
-      />
-      <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
-        <Search />
-      </IconButton>
-    </Paper>
-  )
+  
+  const [searchTerm, setSearchTerm] = useState("");
+
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (searchTerm) {
+			navigate(`/search/${searchTerm}`);
+
+			setSearchTerm("");
+		}
+	};
+
+	return (
+		<Paper
+			component="form"
+			onSubmit={handleSubmit}
+			sx={{
+				borderRadius: 20,
+				sx: "1px solid #000",
+				pl: 2,
+				boxShadow: "none",
+				mr: { sm: 5 },
+			}}>
+			<input
+				type="text"
+				className="search-bar"
+				placeholder="Search"
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
+			<IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+				<Search />
+			</IconButton>
+		</Paper>
+	);
 }
 
 export default SearchBar
