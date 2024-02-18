d1aba63286c93d0c7c3d154802e387ad719e7364
elian-dev <geordymemdoza@gmail.com>
2024-02-17 22:01:43 -0600

    feat: :bug: Fix bug video details view

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -10,7 +10,6 @@ const ChannelDetail = () => {
 
 	const [channelDetail, setChannelDetail] = useState(null);
 	const [videos, setVideos] = useState([]);
-	console.log(channelDetail);
 
 	useEffect(() => {
 		fetchFromApi(`channels?part=snippet&id=${id}`).then((data) =>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,15 @@
 import { Stack, Box } from '@mui/material'
 import { VideoCard, ChannelCard } from './'
 
-const Videos = ({ videos }) => {
-  console.log(videos)
-  return (
-		<Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+const Videos = ({ videos, direction }) => {
+	if (!videos?.length) return "Loading...";
+
+	return (
+		<Stack
+			direction={direction || "row"}
+			flexWrap="wrap"
+			justifyContent="start"
+			gap={2}>
 			{videos.map((item, idx) => (
 				<Box key={idx}>
 					{item.id.videoId && <VideoCard video={item} />}
@@ -13,6 +18,6 @@ const Videos = ({ videos }) => {
 			))}
 		</Stack>
 	);
-}
+};
 
 export default Videos
